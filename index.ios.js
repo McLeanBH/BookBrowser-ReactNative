4d99633912e2989555694eeae803cdfd03081f4a
McLeanBH <mcleanbh@gmail.com>
2015-05-18 14:12:53 -0400

    home interface created and styled...

--- index.ios.js
+++ index.ios.js
@@ -1,37 +1,61 @@
 'use strict';
 
-var React = require('react-native');
+var React = require('react-native'); 
 var {
   AppRegistry,
   View,
+  Text,
+  TextInput,
+  StyleSheet,
 } = React;
 
 var BookBrowser = React.createClass({
   render: function() {
     return (
-      <View>
+      <View style={styles.container}>
+        <Text style={styles.headline}>
+          BookBrowser
+        </Text>
+        <Text style={styles.label}>
+          Find books containing:
+        </Text>
+        <TextInput 
+        placeholder="Search!"
+        style={styles.textInput}/>
       </View>
     );
   }
 });
 
-// var styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F85931',
+  },
+  headline: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#000',
+    marginBottom: 28,
+  },
+  label: {
+    fontSize: 24,
+    fontWeight: 'normal',
+    color: '#FFF',
+    marginBottom: 8,
+  },
+  textInput: {
+    borderColor: '#8e8e93',
+    backgroundColor: '#fff',
+    height: 40,
+    borderWidth: 0.5,
+    marginLeft: 60,
+    marginRight: 60,
+    padding: 8,
+  }
+});
 
 AppRegistry.registerComponent('BookBrowser', () => BookBrowser);
